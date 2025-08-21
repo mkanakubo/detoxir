d1866cf38a8097713e48da87eb04bbd988f2ee70
mkanakubo <masamichi.kanakubo@sansan.com>
2025-08-21 16:42:22 +0900

    set pwa

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,18 @@
+import { Metadata, Viewport } from 'next';
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Detoxir',
+  manifest: '/manifest.json',
+  icons: {
+    icon: '/favicon.ico',
+    apple: [{ url: 'favicon.ico', sizes: '180x180' }],
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: '#000000',
+};
 
 export default function RootLayout({
   children,
@@ -7,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon.ico" />
+      </head>
       <body className={`antialiased`}>{children}</body>
     </html>
   );
