8ea605dc65ffd898337dec342e05a638b5501733
mkanakubo <masamichi.kanakubo@sansan.com>
2025-08-23 00:04:15 +0900

    re-modeling

--- atlas/app/controllers/api/product_identifications_controller.rb
+++ atlas/app/controllers/api/product_identifications_controller.rb
@@ -1,23 +1,13 @@
 # frozen_string_literal: true
 
-class Api::ProductIdentificationsController < ActionController::API
-  before_action :set_default_format
-
+class Api::ProductIdentificationsController < Api::ApplicationController
   def index
     @product_identifications = "Product identifications list"
     # 将来的にはここでデータベースからデータを取得
-    # @product_identifications = ProductIdentification.all
   end
 
   def show
     @product_identification = "Product identification for ID: #{params[:id]}"
     # 将来的にはここでデータベースから特定のデータを取得
-    # @product_identification = ProductIdentification.find(params[:id])
-  end
-
-  private
-
-  def set_default_format
-    request.format = :json unless params[:format]
   end
 end
