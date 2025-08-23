c5497a31a8fed37f9b16dc24c32ed30b31855f5c
HamabeRyu <hamabe-ryu@biz.study-doctor.com>
2025-08-23 20:19:24 +0900

    ログイン画面への遷移を追加

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -1,12 +1,26 @@
 'use client';
 
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useUser } from '@auth0/nextjs-auth0';
 import MeasureStartButton from './_components/MeasureStartButton';
 import DrinkImage from '../_shared/DrinkImage';
 import TimerDisplay from '../_shared/TimerDisplay';
 import Chart from '../_shared/Chart';
 
-
 export default function Home() {
+  const router = useRouter();
+  const { user, isLoading } = useUser();
+
+  useEffect(() => {
+    if (!isLoading) {               // ローディングが終わったら
+      if (!user) {                  // ユーザーがいなければ
+        router.push("/auth/login"); // ログインページへ
+      }
+    }
+  }, [isLoading, user, router]);
+
+  // ログイン済みの場合だけ本来の画面を表示
   return (
     <main>
       <MeasureStartButton />
@@ -18,7 +32,6 @@ export default function Home() {
       >
         <DrinkImage />
         <TimerDisplay />
-        
       </section>
       <Chart />
     </main>
